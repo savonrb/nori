3cdc7be52401207f32d209a6c836f882f3b08a32
rubiii <me@rubiii.com>
2011-02-27 20:30:00 +0100

    added benchmarking and a spec/test rake task

--- Rakefile
+++ Rakefile
@@ -1,2 +1,16 @@
 require "bundler"
 Bundler::GemHelper.install_tasks
+
+require "rspec/core/rake_task"
+
+desc "Benchmark Nori parsers"
+task :benchmark do
+  require "benchmark/benchmark"
+end
+
+RSpec::Core::RakeTask.new do |t|
+  t.rspec_opts = %w(-fd -c)
+end
+
+task :default => :spec
+task :test => :spec
---
+++ benchmark/benchmark.rb
@@ -0,0 +1,19 @@
+$:.push File.expand_path("../../lib", __FILE__)
+require "nori"
+
+require "benchmark"
+
+Benchmark.bm 30 do |x|
+
+  num = 250
+  xml = File.read File.expand_path("../soap_response.xml", __FILE__)
+
+  x.report "rexml parser" do
+    num.times { Nori.parse xml, :rexml }
+  end
+
+  x.report "nokogiri parser" do
+    num.times { Nori.parse xml, :nokogiri }
+  end
+
+end

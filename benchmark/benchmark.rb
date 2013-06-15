c51eaa6554dba569edd6499d5fadee5d90918f68
robuye <rulejczyk@gmail.com>
2013-06-15 10:43:04 +0000

    Update benchmarks to work with Nori 2.x.x

--- benchmark/benchmark.rb
+++ benchmark/benchmark.rb
@@ -9,11 +9,11 @@ Benchmark.bm 30 do |x|
   xml = File.read File.expand_path("../soap_response.xml", __FILE__)
 
   x.report "rexml parser" do
-    num.times { Nori.parse xml, :rexml }
+    num.times { Nori.new(parser: :rexml).parse xml }
   end
 
   x.report "nokogiri parser" do
-    num.times { Nori.parse xml, :nokogiri }
+    num.times { Nori.new(parser: :nokogiri).parse xml }
   end
 
 end
